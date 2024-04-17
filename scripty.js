ef65008ad98e0639b4668198f47eeec489ba27ec
Charlie Roberts <charlie@robertsclan.co.uk>
2024-04-17 19:58:14 +0100

    Smooth scrolling js

--- scripty.js
+++ scripty.js
@@ -1,1 +1,13 @@
-
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        const offset = target.offsetTop - 50; // Adjusted offset
+        window.scrollTo({
+          top: offset,
+          behavior: 'smooth'
+        });
+      });
+    });
+  });
