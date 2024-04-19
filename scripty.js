0dc0538ee8630bfa34bc63e10c69a1b25485db4c
Charlie Roberts <charlie@robertsclan.co.uk>
2024-04-19 14:55:21 +0100

    Smooth scrolling still not working

--- scripty.js
+++ scripty.js
@@ -3,10 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
     anchor.addEventListener('click', function (e) {
       e.preventDefault();
       const target = document.querySelector(this.getAttribute('href'));
-      const offset = target.offsetTop - 50; // Adjusted offset
-      target.scrollIntoView({
-        behavior: 'smooth'
-      });
+      target.scrollIntoView({ behavior: 'smooth', block: 'start' });
     });
   });
- }); 
+ });
